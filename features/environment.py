bcd32e45aabb1147c3098b4dbbafe4252418a82b
Akopov4 <akopov4@gmail.com>
2023-11-13 11:50:32 +0200

    updated env file

--- features/environment.py
+++ features/environment.py
@@ -1,24 +1,37 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 from behave import fixture, use_fixture
 
 
-@fixture()
-def browser(context):
-    context.browser = webdriver.Chrome(ChromeDriverManager().install())
+class WebdriverManager:
+    def __init__(self, browser_name):
+        self.browser_name = browser_name
+        self.driver = None
+
+    def get_webdriver_instance(self):
+        if self.browser_name.lower() == "chrome":
+            self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        elif self.browser_name.lower() == "firefox":
+            self.driver = webdriver.Firefox(GeckoDriverManager.install())
+        else:
+            raise ValueError(f"Unsupported browser: {self.browser_name}")
+        return self.driver
+
+    def quit(self):
+        if self.driver is not None:
+            self.driver.quit()
+
+
+
+
+
+def before_all(context,scenario):
+    browser_name = getattr(context, 'browser', 'chrome')
+    context.web_driver_manager = WebdriverManager(browser_name)
+    context.browser = context.web_driver_manager.get_webdriver_instance()
     context.browser.maximize_window()
-    yield context.browser
-    context.browser.quit()
-
-
-@fixture()
-def open_home_page(context):
     context.browser.get('https://aibuy.io/')
 
-
-def before_all(context):
-    use_fixture(browser,context)
-
-
-def before_scenario(context,scenario):
-    use_fixture(open_home_page,context)
+def after_all(context):
+    context.browser.quit()
